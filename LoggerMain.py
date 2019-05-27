776084de04e7e832976f1fb07c8bf3a00367c36f
Frederik <fredi-petersen93@hotmail.com>
2019-05-27 21:30:09 +0200

    changed stuff

--- LoggerMain.py
+++ LoggerMain.py
@@ -6,7 +6,7 @@ vulnPorts = ["21", "22", "23", "25", "53", "80", "110", "111", "135", "139", "14
 portPattern = re.compile(r'Destination Port: 21|Destination Port: 23|Destination Port: 25|Destination Port: 53|Destination Port: 80|Destination Port: 110|Destination Port: 111|Destination Port: 135|Destination Port: 139|Destination Port: 143|Destination Port: 445|Destination Port: 993|Destination Port: 995|Destination Port: 1723|Destination Port: 3306|Destination Port: 3389|Destination Port: 5900|Destination Port: 8080')
 tcpDictionary = Counter({"SYN": 0, "RST, ACK": 0, "ACK": 0})
 
-f = open("realHandshake.txt", "r")
+f = open("exampleWireshark1.txt", "r")
 clearTxt = f.readlines()
 
 
@@ -36,10 +36,12 @@ def gatherPorts(txt):
     result = portPattern.findall(str(txt))
     print(result)
     if len(result) > 50:
+        print(len(result))
+        print(tcpDictionary)
         print("port sweep detected, Contact adinistrator")
     else:
         print("no port sweep detected")
 
 
-# synAckLookup(cleartxt)
+synAckLookup(clearTxt)
 gatherPorts(clearTxt)
