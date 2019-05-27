277c9f3fcbf152cc031010521147692990c6a91e
Frederik <fredi-petersen93@hotmail.com>
2019-05-27 22:45:48 +0200

    ip search

--- LoggerMain.py
+++ LoggerMain.py
@@ -1,12 +1,14 @@
 import re
 from collections import Counter
+import socket
 
 vulnPorts = ["21", "22", "23", "25", "53", "80", "110", "111", "135", "139", "143", "443", "445", "993", "995", "1723",
              "3306", "3389", "5900", "8080"]
 portPattern = re.compile(r'Destination Port: 21|Destination Port: 23|Destination Port: 25|Destination Port: 53|Destination Port: 80|Destination Port: 110|Destination Port: 111|Destination Port: 135|Destination Port: 139|Destination Port: 143|Destination Port: 445|Destination Port: 993|Destination Port: 995|Destination Port: 1723|Destination Port: 3306|Destination Port: 3389|Destination Port: 5900|Destination Port: 8080')
+ipList = []
 tcpDictionary = Counter({"SYN": 0, "RST, ACK": 0, "ACK": 0})
 
-f = open("exampleWireshark1.txt", "r")
+f = open("portSweep.txt", "r")
 clearTxt = f.readlines()
 
 
@@ -31,17 +33,20 @@ def synAckLookup(txt):
                     tcpDictionary.update({"ACK": 1})
                 else:
                     break
-
 def gatherPorts(txt):
     result = portPattern.findall(str(txt))
     print(result)
     if len(result) > 50:
         print(len(result))
-        print(tcpDictionary)
         print("port sweep detected, Contact adinistrator")
     else:
         print("no port sweep detected")
+def gatherIp(txt):
+    ownName = socket.gethostbyname_ex(socket.gethostname())
+    ipPattern = re.compile(r"[0-9]+(?:\.[0-9]+){3}")
+    result = ipPattern.findall(str(txt))
+    ipList.append(result)
 
 
-synAckLookup(clearTxt)
-gatherPorts(clearTxt)
+gatherIp(clearTxt)
+print(ipList)
