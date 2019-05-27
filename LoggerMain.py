e33cc5c1691b086074f3e409a223fd67f5a21fea
Frederik <fredi-petersen93@hotmail.com>
2019-05-27 16:25:08 +0200

    changed text

--- LoggerMain.py
+++ LoggerMain.py
@@ -8,7 +8,8 @@ f = open(txt, "r")
 cleartxt = f.read()
 x = re.search("ganske", cleartxt)
 
-if (x):
-  print("YES! We have a match!")
-else:
-  print("No match")
+def testFile(x):
+  if (x):
+    print("YES! We have a match!")
+  else:
+    print("No match")
