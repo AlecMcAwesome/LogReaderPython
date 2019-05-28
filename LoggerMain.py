35ba1005d3c96fe7cc43bb62616a6bc17f90c730
Frederik <fredi-petersen93@hotmail.com>
2019-05-28 17:39:29 +0200

    done

--- LoggerMain.py
+++ LoggerMain.py
@@ -1,52 +1,64 @@
 import re
-from collections import Counter
 import socket
+from collections import Counter
 
-vulnPorts = ["21", "22", "23", "25", "53", "80", "110", "111", "135", "139", "143", "443", "445", "993", "995", "1723",
-             "3306", "3389", "5900", "8080"]
-portPattern = re.compile(r'Destination Port: 21|Destination Port: 23|Destination Port: 25|Destination Port: 53|Destination Port: 80|Destination Port: 110|Destination Port: 111|Destination Port: 135|Destination Port: 139|Destination Port: 143|Destination Port: 445|Destination Port: 993|Destination Port: 995|Destination Port: 1723|Destination Port: 3306|Destination Port: 3389|Destination Port: 5900|Destination Port: 8080')
-ipList = []
-tcpDictionary = Counter({"SYN": 0, "RST, ACK": 0, "ACK": 0})
+portPattern = re.compile(
+    r'Destination Port: 21|Destination Port: 23|Destination Port: 25|Destination Port: 53|Destination Port: 80|Destination Port: 110|Destination Port: 111|Destination Port: 135|Destination Port: 139|Destination Port: 143|Destination Port: 445|Destination Port: 993|Destination Port: 995|Destination Port: 1723|Destination Port: 3306|Destination Port: 3389|Destination Port: 5900|Destination Port: 8080')
+portList = set()
+ipList = set()
+nrIps = Counter({})
 
-f = open("portSweep.txt", "r")
-clearTxt = f.readlines()
+path = input("please type path to log (must be txt file): ")
+log = open(path, "r")
+clearTxt = log.readlines()
+resume = open("Resume.txt", "w+")
 
 
-def synAckLookup(txt):
-    keywords = ["SYN", "RST, ACK", "ACK"]
-    for line in txt:
-        for word in keywords:
-            if word in line:
-                synvar = word.count("SYN")
-                if synvar == 1:
-                    tcpDictionary.update({"SYN": 1})
-                else:
-                    break
-                rstAckVar = word.count("RST")
-                if rstAckVar <= 0:
-                    tcpDictionary.update({"RST, ACK": 1})
-                else:
-                    break
-
-                ackVar = word.count("ACK")
-                if ackVar <= 0:
-                    tcpDictionary.update({"ACK": 1})
-                else:
-                    break
 def gatherPorts(txt):
+    resume.write(
+        "Resume of vulnerable destination ports from log. if logs show vulnerable ports here\n"
+        "please contact administrator since there could be an incoming attack and measures must be taken \n"
+        "Ports: \n")
     result = portPattern.findall(str(txt))
-    print(result)
-    if len(result) > 50:
-        print(len(result))
+    for item in result:
+        portList.update({item: 1})
+    if len(portList) > 15:
+        print(len(portList))
         print("port sweep detected, Contact adinistrator")
+        for port in portList:
+            resume.write("%s \n" % port)
     else:
         print("no port sweep detected")
+
+
 def gatherIp(txt):
-    ownName = socket.gethostbyname_ex(socket.gethostname())
-    ipPattern = re.compile(r"[0-9]+(?:\.[0-9]+){3}")
-    result = ipPattern.findall(str(txt))
-    ipList.append(result)
+    ippattern = re.compile(r"[0-9]+(?:\.[0-9]+){3}")
+    result = ippattern.findall(str(txt))
+    resume.write(
+        "\nList of ip adresses and number of occurances. ip adresses and ports should be cross refrenced to se where \n"
+        "portsweep might come from:\n \n")
+    for item in result:
+        ipList.add(item)
+        nrIps.update({item: 1})
+
+    resume.write("Your ip andress/adresses are the following:")
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    resume.write("\nComputer name: {0} \nIP Address: {1} \n\n".format(hostname, IPAddr))
+
+    resume.write("\nIP addresses and number of occurrences:\n")
+    for key, value in nrIps.items():
+        resume.write("IP: {0}  Number of Occurrences: {1} \n".format(key, value))
 
 
+def verdict():
+    resume.write("\n \nFinal Verdict of log reader: \n\n")
+    if len(portList) >= 15:
+        resume.write("ports indicate sweep! contact administrator!!")
+    else:
+        resume.write("logs does not indicate any port sweep attack. Carry on soldier!")
+
+
+gatherPorts(clearTxt)
 gatherIp(clearTxt)
-print(ipList)
+verdict()
